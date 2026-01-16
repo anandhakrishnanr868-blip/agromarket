b0ce775d192b66f783180beb5d0e5a40b6e1047a
anandhakrishnanr868-blip <anandhakrishnanr868@gmail.com>
2026-01-16 20:04:15 +0530

    Update security.py

--- security.py
+++ security.py
@@ -1,4 +1,5 @@
 from argon2 import PasswordHasher
+import base64
 
 ph = PasswordHasher()
 
@@ -7,3 +8,10 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, hashed: str) -> bool:
     return ph.verify(hashed, password)
+
+def encode_response(value:str)->str:
+    return base64.urlsafe_b64encode(str(value).encode()).decode()
+
+
+def decode_response(value:str)->str:
+    return base64.urlsafe_b64decode(value.encode()).decode()
