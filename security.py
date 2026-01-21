07ea364dbc55a1e145c6a354f45cd53832aa6281
anandhakrishnanr868-blip <anandhakrishnanr868@gmail.com>
2026-01-22 01:26:27 +0530

    Update security.py

--- security.py
+++ security.py
@@ -1,4 +1,5 @@
 from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
 import base64
 
 ph = PasswordHasher()
@@ -7,11 +8,19 @@ def hash_password(password: str) -> str:
     return ph.hash(password)
 
 def verify_password(password: str, hashed: str) -> bool:
-    return ph.verify(hashed, password)
+    try:
+        return ph.verify(hashed, password)
+    except VerifyMismatchError:
+        return False
 
-def encode_response(value:str)->str:
-    return base64.urlsafe_b64encode(str(value).encode()).decode()
+def encode_response(value: str) -> str:
+    try:
+      return base64.urlsafe_b64encode(value.encode()).decode()
+    except Exception:
+        return None
 
-
-def decode_response(value:str)->str:
-    return base64.urlsafe_b64decode(value.encode()).decode()
+def decode_response(value: str) -> str:
+    try:
+     return base64.urlsafe_b64decode(value.encode()).decode()
+    except Exception:
+        return None
